bd6cfcb2dd7d9e308f79861438e4d650a30c9cb6
unknown <jinyoung_lin@hunt.com.tw>
2021-05-05 09:03:29 +0800

    Initial commit

---
+++ src/main/java/com/jin/Hello.java
@@ -0,0 +1,9 @@
+package com.jin;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println();
+       Person p = new Person();
+       p.hello();
+    }
+}
---
+++ src/main/java/com/jin/Person.java
@@ -0,0 +1,7 @@
+package com.jin;
+
+public class Person {
+    public void hello() {
+        System.out.println("hello world");
+    }
+}
